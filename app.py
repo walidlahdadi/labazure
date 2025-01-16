5bd4adebb949498054f4e4405e0583e28d459a2e
walidlahdadi <145559816+walidlahdadi@users.noreply.github.com>
2025-01-16 23:07:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 import math
-import os  # Import to handle environment variables
 
 app = Flask(__name__)
 
@@ -23,6 +22,4 @@ def compute_integral(lower, upper):
     return jsonify(results)
 
 if __name__ == '__main__':
-    # Azure sets the PORT environment variable. Default to 5000 for local testing.
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
